10ccc92b04440f33312e24cc033760144c7da593
Fue Idriss Shulu <61651516+Jitomorin@users.noreply.github.com>
2023-11-07 21:01:16 +0300

    added line spacing

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -8,10 +8,10 @@ export default function Slider() {
   const ImageWrapper = styled.div`
     background: url("/stock-image-1.jpg");
     background-size: cover;
+    background-position: center;
     ${media("<=desktop")} {
       background-size: cover;
       background-repeat: no-repeat;
-      background-position: center;
     }
   `;
 
