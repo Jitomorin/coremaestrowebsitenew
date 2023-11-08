0c98135e57826259472bcf4827862831be5b02a1
Fue Idriss Shulu <61651516+Jitomorin@users.noreply.github.com>
2023-11-08 15:17:38 +0300

    Added privacy policy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,6 +22,7 @@ const footerItems: FooterItems = [
       { title: "Payroll Management System", href: "/services" },
       { title: "Learning Management System", href: "/services" },
       { title: "ATS Job Board", href: "/services" },
+      { title: "Privacy Policy", href: "/services" },
     ],
   },
   {
